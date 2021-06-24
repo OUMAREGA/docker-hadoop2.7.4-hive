cb9557e5c2ef614118790c64d15c6271a1e5af7c
ii02735 <yadallee.bilaal@outlook.fr>
2021-06-24 22:52:38 +0200

    [MAP/REDUCE] Generic mapper

---
+++ map_reduce/temp_max_jour.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Déterminer l'écart de température par jour
+# DATE TMP 
+
+from datetime import datetime
+import sys
+
+
+dico = {}
+for line in sys.stdin:
+    date,temp = line.split('\t')
+    temp=float(temp.replace(',','.'))
+    # 2021-06-21T21:00:00
+    day = date.split('T')
+    if day[0] not in dico:
+        dico[day[0]] = []
+    dico[day[0]].append(temp);
+
+for day in dico.keys():
+    dico[day].sort()
+    print(f"{day}\t{dico[day][-1]}")
