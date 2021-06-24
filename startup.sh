b01705f2c3fa8713718188322ad16fdefb02430a
oumarega <oumarmarega18@gmail.com>
2021-06-24 10:44:45 +0200

    create data folder - startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,8 @@ hadoop fs -mkdir       /tmp
 hadoop fs -mkdir -p    /user/hive/warehouse
 hadoop fs -chmod g+w   /tmp
 hadoop fs -chmod g+w   /user/hive/warehouse
+hadoop fs -mkdir -p    /user/hadoop/data
+hadoop fs -chmod g+w    /user/hadoop/data
 
 cd $HIVE_HOME/bin
 ./hiveserver2 --hiveconf hive.server2.enable.doAs=false
