9c8b2dfed497a81f456cd1a9e61ae6c5c3b04329
ii02735 <yadallee.bilaal@outlook.fr>
2021-06-25 13:13:36 +0200

    [WIP] mongo connector integration

--- data/fetch-last.sh
+++ data/fetch-last.sh
@@ -2,7 +2,15 @@
 
 [ -z $1 ] && echo "No year provided" && exit 1;
 
-curl -s https://www.ncei.noaa.gov/data/global-hourly/access/$1/ | sed -r "s/.*\"(.*csv)\".*/\1/g" | grep .csv | tail -n 1
+last_csv=$(curl -s https://www.ncei.noaa.gov/data/global-hourly/access/$1/ | sed -r "s/.*\"(.*csv)\".*/\1/g" | grep .csv | tail -n 1)
+last_local_csv=$(cat last_$1_file_downloaded.txt) 
+if [ "$last_local_csv" != "$last_csv" ]
+then
+	echo "needs to be synced"
+	wget https://www.ncei.noaa.gov/data/global-hourly/access/$1/$last_csv
+else
+	echo "nothing to do"
+fi
 
 
 # utiliser tail -n 1 pour récupérer la dernière ligne de la requête et comparer le nom de l'ancien fichier csv avec le nouveau
--- data/fetch.sh
+++ data/fetch.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 [ -z $1 ] && echo "No year provided" && exit 1
-csv_list=$(curl -s https://www.ncei.noaa.gov/data/global-hourly/access/$1/ | sed -r "s/.*\"(.*csv)\".*/\1/g" | grep .csv)
+curl -s https://www.ncei.noaa.gov/data/global-hourly/access/$1/ | sed -r "s/.*\"(.*csv)\".*/\1/g" | grep ".csv" > csv_list
 if [ ! -z $2 ]
 then
-	csv_list=$($csv_list | tail -n $2)
+       echo "provided"
+       cat csv_list | tail -n $2 > csv_list_short
 fi
-echo $csv_list
-exit
-for line in $csv_list
+mv csv_list_short csv_list
+for line in $(cat ./csv_list)
 do
 	wget https://www.ncei.noaa.gov/data/global-hourly/access/$1/$line
 done
-# utiliser tail -n 1 pour récupérer la dernière ligne de la requête et comparer le nom de l'ancien fichier csv avec le nouveau
+cat csv_list | tail -n 1 > last_$1_file_downloaded.txt
+rm csv_list
 
 #while read line;
 #do
